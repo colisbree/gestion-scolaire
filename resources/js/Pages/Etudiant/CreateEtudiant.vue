072e948a3edb656c1828d5d4319a6b10d4871443
Denis <d.combe@laposte.net>
2023-01-03 15:23:46 +0100

    affichage liste des étudiants et pagination

---
+++ resources/js/Pages/Etudiant/CreateEtudiant.vue
@@ -0,0 +1,4 @@
+<template>  
+    <button class="btn btn-primary"  data-toggle="modal" data-target="#CreateNVModal"><i class="fa fa-plus"></i> Nouveau</button>
+     
+</template>
---
+++ resources/js/Pages/Etudiant/IndexEtudiant.vue
@@ -0,0 +1,70 @@
+<template>  
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Liste des Etudiants</h1>
+                </div>  
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">
+                                <button class="btn btn-primary" ><i class="fa fa-plus"></i> Nouveau</button>
+                            </div>
+                            <div class="card-tools">
+                                <PaginationVue 
+                                    :links="props.etudiants.links" 
+                                    :prev="props.etudiants.prev_page_url"
+                                    :next="props.etudiants.next_page_url"
+                                />
+                            </div>
+                        </div>
+
+                        <div class="card-body p-0">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Etudiant</th>
+                                        <th>Niveau Scolaire</th>
+                                        <th style="width: 100px"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="etudiant in props.etudiants.data" :key="etudiant">
+                                        <td>{{ etudiant.nom }} {{ etudiant.prenom }}</td>
+                                        <td>{{ etudiant.niveau_scolaire.nom }} </td>
+                                        <td>
+                                            <div class="d-flex justify-items-center">
+                                                <button  class="btn btn-info mr-2"><i class="fas fa-pen"></i></button>
+                                                <button  class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup> 
+    import PaginationVue from "@/Shared/Pagination.vue"
+    //import CreateEtudiant from "./CreateEtudiant.vue";
+
+    const props = defineProps({
+        etudiants: Object
+    })
+</script>
